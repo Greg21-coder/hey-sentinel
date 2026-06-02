4fc7cf1dd4071db07afd2ed584aa28d3ab43c6cb
Greg <greg.altuve@gmail.com>
2026-06-02 10:28:02 -0400

    feat(customer): add My Apps to sidebar navigation

--- resources/js/Layouts/CustomerLayout.tsx
+++ resources/js/Layouts/CustomerLayout.tsx
@@ -16,6 +16,11 @@ const navItems: NavItem[] = [
         icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6',
     },
     {
+        href: '/customer/my-apps',
+        label: 'My Apps',
+        icon: 'M5 13l4 4L19 7',
+    },
+    {
         href: '/customer/apps',
         label: 'Browse Apps',
         icon: 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z',
